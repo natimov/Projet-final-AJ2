93f56b951ccfb36b2af7e313041faecf90a8e249
natimov <tim.neninautexier@gmail.com>
2026-09-03 20:10:41 +0200

    feat(users): ajoute le CRUD Utilisateurs (API)

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
@@ -11,3 +11,5 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
